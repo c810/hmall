586552b4ebbeec0cbb192f2204bfe6432ddae5e7
lihaopeng <752869332@qq.com>
2024-10-23 21:22:00 +0800

    feat: 网关登录校验 - OpenFeign传递用户信息

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,6 +1,9 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -17,5 +20,27 @@ public class DefaultFeignConfig {
         return Logger.Level.NONE;
         // 上线时可以开启FULL日志级别
         // return Logger.Level.FULL;
+    }
+
+    /**
+     * feign拦截器，用于微服务之间传递用户信息
+     *
+     * @return RequestInterceptor 拦截器
+     */
+    @Bean
+    public RequestInterceptor userInfoRequestInterceptor(){
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                // 获取登录用户
+                Long userId = UserContext.getUser();
+                if(userId == null) {
+                    // 如果为空则直接跳过
+                    return;
+                }
+                // 如果不为空则放入请求头中，传递给下游微服务
+                template.header("user-info", userId.toString());
+            }
+        };
     }
 }

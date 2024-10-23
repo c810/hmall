65f352b503396f9bacfef7dad3a7b07ec1407266
lihaopeng <752869332@qq.com>
2024-10-23 19:49:18 +0800

    feat: 网关路由 - 快速入门

---
+++ hm-gateway/src/main/java/com/hmall/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lihaopeng
+ * @version 1.0
+ * @project hmall
+ * @description
+ * @since 2024/10/23 19:37
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
